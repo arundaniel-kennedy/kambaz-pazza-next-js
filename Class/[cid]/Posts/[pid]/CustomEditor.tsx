ca961d1f98fabec9d4b24869d3140e9680c4a44f
arthsindekar <sindekar.a@northeastern.edu>
2025-12-07 19:28:00 -0500

    changed editor

--- Class/[cid]/Posts/[pid]/CustomEditor.tsx
+++ Class/[cid]/Posts/[pid]/CustomEditor.tsx
@@ -1,20 +1,5 @@
 import React from "react";
-import {
-  EditorProvider,
-  Editor,
-  Toolbar,
-  BtnUndo,
-  BtnRedo,
-  BtnBold,
-  BtnItalic,
-  BtnUnderline,
-  BtnStrikeThrough,
-  BtnNumberedList,
-  BtnBulletList,
-  BtnLink,
-  BtnClearFormatting,
-  createButton,
-} from "react-simple-wysiwyg";
+import Editor from "react-simple-wysiwyg";
 import { setPosts } from "../../reducer";
 import { Posts } from "../../DataStructure";
 import { useDispatch } from "react-redux";
@@ -27,94 +12,32 @@ export default function CustomEditor({
   content?: string;
 }) {
   const dispatch = useDispatch();
-  const BtnAlignCenter = createButton("Align center", "≡", "justifyCenter");
-  const BtnAlignLeft = createButton("Align left", "⭠", "justifyLeft");
-  const BtnAlignRight = createButton("Align right", "⭢", "justifyRight");
-  const BtnImage = createButton("Insert image", "🖼️", "insertImage");
+
   return (
     <div>
       {" "}
       {content === "summary" && (
-        <EditorProvider>
-          <Editor
-            containerProps={{ style: { resize: "vertical" } }}
-            value={`${post?.summary}`}
-            onChange={(e) =>
-              dispatch(setPosts({ ...post, summary: e.target.value }))
-            }
-          >
-            <Toolbar>
-              <BtnUndo />
-              <BtnRedo />
-              <BtnBold />
-              <BtnItalic />
-              <BtnUnderline />
-              <BtnStrikeThrough />
-              <BtnNumberedList />
-              <BtnBulletList />
-              <BtnAlignLeft />
-              <BtnAlignCenter />
-              <BtnAlignRight />
-              <BtnLink />
-              <BtnImage />
-              <BtnClearFormatting />
-            </Toolbar>
-          </Editor>
-        </EditorProvider>
+        <Editor
+          containerProps={{ style: { resize: "vertical" } }}
+          value={`${post?.summary}`}
+          
+        />
       )}
       {content === "details" && (
-        <EditorProvider>
-          <Editor
-            containerProps={{ style: { resize: "vertical" } }}
-            value={`${post?.details}`}
-            onChange={(e) =>
-              dispatch(setPosts({ ...post, details: e.target.value }))
-            }
-          >
-            <Toolbar>
-              <BtnUndo />
-              <BtnRedo />
-              <BtnBold />
-              <BtnItalic />
-              <BtnUnderline />
-              <BtnStrikeThrough />
-              <BtnNumberedList />
-              <BtnBulletList />
-              <BtnAlignLeft />
-              <BtnAlignCenter />
-              <BtnAlignRight />
-              <BtnLink />
-              <BtnImage />
-              <BtnClearFormatting />
-            </Toolbar>
-          </Editor>
-        </EditorProvider>
+        <Editor
+          containerProps={{ style: { resize: "vertical" } }}
+          value={`${post?.details}`}
+          
+        />
       )}
-      {(!content) && (<EditorProvider>
-          <Editor
-            containerProps={{ style: { resize: "vertical" } }}
-            onChange={(e) =>
-              dispatch(setPosts({ ...post, answer: e.target.value }))
-            }
-          >
-            <Toolbar>
-              <BtnUndo />
-              <BtnRedo />
-              <BtnBold />
-              <BtnItalic />
-              <BtnUnderline />
-              <BtnStrikeThrough />
-              <BtnNumberedList />
-              <BtnBulletList />
-              <BtnAlignLeft />
-              <BtnAlignCenter />
-              <BtnAlignRight />
-              <BtnLink />
-              <BtnImage />
-              <BtnClearFormatting />
-            </Toolbar>
-          </Editor>
-        </EditorProvider>)}
+      {!content && (
+        <Editor
+          containerProps={{ style: { resize: "vertical" } }}
+          onChange={(e) =>
+            dispatch(setPosts({ ...post, answer: e.target.value }))
+          }
+        />
+      )}
     </div>
   );
 }
